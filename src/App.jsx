f2ed5a662c36de38d52f4b8c672bcbab02b4d407
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-19 17:10:49 +0500

    initialize React app with Vite, add basic game components and styles

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import MemoryCard from "./components/MemoryCard";
+import Form from "./components/Form";
+
+export default function App() {
+  const [isGameOn, setIsGameOn] = useState(false);
+
+  function startGame(e) {
+    e.preventDefault();
+    setIsGameOn(true);
+  }
+
+  function turnCard() {
+    console.log("Memory card clicked");
+  }
+
+  return (
+    <main>
+      <h1>Memory</h1>
+      {!isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} />}
+    </main>
+  );
+}
