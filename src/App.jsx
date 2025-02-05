53a124259f2bbe51e292b001aa28bbc085f4e321
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-02-05 17:11:18 +0500

    update title to 'Memory Game' and implement emoji selection and shuffling logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Form from "./components/Form";
-import MemoryCard from './components/MemoryCard'
+import MemoryCard from "./components/MemoryCard";
 
 export default function App() {
   const [isGameOn, setIsGameOn] = useState(false);
@@ -19,13 +19,53 @@ export default function App() {
       }
 
       const data = await response.json();
-      const dataSample = data.slice(0, 5);
+      const dataSlice = getDataSlice(data);
+      const emojisArray = getEmojisArray(dataSlice);
 
-      setEmojisData(dataSample);
+      setEmojisData(emojisArray);
       setIsGameOn(true);
     } catch (err) {
       console.error(err);
     }
+  }
+
+  function getDataSlice(data) {
+    const randomIndices = getRandomIndices(data);
+
+    const dataSlice = randomIndices.reduce((array, index) => {
+      array.push(data[index]);
+      return array;
+    }, []);
+
+    return dataSlice;
+  }
+
+  function getRandomIndices(data) {
+    const randomIndicesArray = [];
+
+    for (let i = 0; i < 5; i++) {
+      const randomNum = Math.floor(Math.random() * data.length);
+      if (!randomIndicesArray.includes(randomNum)) {
+        randomIndicesArray.push(randomNum);
+      } else {
+        i--;
+      }
+    }
+
+    return randomIndicesArray;
+  }
+
+  function getEmojisArray(data) {
+    const pairedEmojisArray = [...data, ...data];
+
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = pairedEmojisArray[i];
+      pairedEmojisArray[i] = pairedEmojisArray[j];
+      pairedEmojisArray[j] = temp;
+    }
+
+    return pairedEmojisArray;
   }
 
   function turnCard() {
